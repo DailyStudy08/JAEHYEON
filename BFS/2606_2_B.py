c95a5193f7729659f529e696c3e9f89f12c291aa
JAY-Winter <rubat0@naver.com>
2022-09-09 04:13:25 +0900

    백준2606 : 바이러스

---
+++ BFS/2606_2_B.py
@@ -0,0 +1,20 @@
+N = int(input())
+M = int(input())
+adj_lst = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    adj_lst[a].append(b)
+    adj_lst[b].append(a)
+
+visited = [0] * (N + 1)
+cnt = 0
+Q = [1]
+while Q:
+    w = Q.pop(0)
+    cnt += 1
+    for node in adj_lst[w]:
+        if visited[node] == 0:
+            visited[node] = 1
+            Q.append(node)
+
+print(cnt)
